7c0c32a52001fa24f4048460995c4ff6f44b8bb0
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-03-05 11:09:03 +0900

    fix

--- docker-pull.sh
+++ docker-pull.sh
@@ -14,18 +14,19 @@ docker_save() {
   docker save "${name}" > "${fn}"
 }
 
-docker_load_if_available() {
+docker_load_or_pull() {
   local name=$1
   local fn="${DOCKER_CACHE}/$(docker_name_to_basename "${name}")"
   if [[ -e "${fn}" ]]; then
+    echo "loading from ${fn}"
     docker load -i "${fn}"
   else
-    return 1
+    docker pull "${name}"
   fi
 }
 
 for i in ubuntu:14.10 golang:1.4 postgres:9.4 redis:2.8 progrium/consul:consul-0.4 nabeken/docker-volume-container-rsync:latest; do
   echo "$(LANG=C date): begin for ${i}"
-  docker_load_if_available "${i}" || docker_save "${i}"
+  docker_load_or_pull "${i}" || docker_save "${i}"
   echo "$(LANG=C date): done for ${i}"
 done
