054b310c22055d42f882200f57bd0a1186a60b1d
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-03-07 13:59:14 +0900

    fix

--- functions.sh
+++ functions.sh
@@ -2,7 +2,7 @@
 DOCKER_CACHE="${HOME}/docker2"
 
 docker_name_to_basename() {
-  echo $(echo "${1}" | tr ':/' '-').tar.gz
+  echo $(echo "${1}" | tr ':/' '-').tar
 }
 
 docker_save_if_necessary() {
@@ -10,7 +10,7 @@ docker_save_if_necessary() {
   local fn="${DOCKER_CACHE}/$(docker_name_to_basename "${name}")"
   if [[ ! -e "${fn}" ]]; then
     echo "saving to ${fn}"
-    docker save "${name}" | gzip > "${fn}"
+    docker save -o "${fn}" "${name}"
   fi
 }
 
